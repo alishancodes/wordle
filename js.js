157ef3e13ed694c43b35bdf7851e5639ebfe4231
alishancodes <alishan2871@gmail.com>
2026-08-08 19:19:36 +0000

    updated get guess logic

--- js.js
+++ js.js
@@ -1,8 +1,9 @@
 //selects a word randomly which are greater than 2 letters
-const fs = require('fs');
+async function getRandomWord() {
 
-function getRandomWord() {
-    const data = fs.readFileSync('words.txt', 'utf8');
+    const response = await fetch('words.txt');
+
+    const data = await response.text();
 
     const words = data.split('\n');
 
@@ -17,32 +18,62 @@ function getRandomWord() {
 
 // function to take guess
 
-function getGuess(limit) {
+function getGuess(limit, length) {
+    let guess;
 
-    let guess = prompt(`Enter you guess !! [its a word with more than ${limit} letters\n so you get ${limit - 1} tries]\n`);
+    do {
+        guess = prompt(`Enter your guess!! [it's a word with ${length} letters\nso you get ${limit - 2} tries]`).toLowerCase();
 
-    return guess.toLowerCase();
+        if (guess.length !== length) {
+            alert(`Enter a word with ${length} letters`);
+        }
+        else if (!words.includes(guess)) {
+            alert(`"${guess}" is not a valid word`);
+        }
+        else {
+            break;
+        }
 
+    } while (true);
 }
 
 // function to compare the guess and secretWord
 
-function compare(guess, secretword) {
+function compare(guess, secretWord) {
     let i = 0, j = 0, correctCounter = 0;
     let arr = [];
+    let index = [];
 
-    for (i; i < secretWord.length(); i++) {
-        for (j; j < secretWord.length(); i++) {
-            if(secretWord[i]==guess[j]){
-                arr=secretWord[i];
-                correctCounter++;
-            }
+    for (i; i < secretWord.length; i++) {
+        if (guess.includes(secretWord[i])) {
+            arr[j] = secretWord[i]; //stores the letter matched 
+            index[j] = i; //stores the index of correct letter
+            correctCounter++; //count how many letters matched
+            j++;
         }
     }
 
-    console.log(`you got ${correctCounter} letters which are ${arr}`);
+
+    console.log(`you got ${correctCounter} letters which are ${arr}\n`);
 
 }
+
+//function to start the game and decide the rounds to be played
+
+async function startGame() {
+    const secretWord = await getRandomWord();
+    let i = 0;
+    let limit = secretWord.length;
+    for (i; i < limit; i++) {
+        const user_input = getGuess(limit,secretWord.length);
+        compare(user_input, secretWord);
+        limit--;
+    }
+
+}
+
+startGame();
+
 
 
 
