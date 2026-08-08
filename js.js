92685c45fbf38afa094554a4d5c9c9cf681cb257
alishancodes <alishan2871@gmail.com>
2026-08-08 17:43:47 +0000

    function to select an random wordle

--- js.js
+++ js.js
@@ -1,3 +1,19 @@
+//selects a word randomly which are greater than 2 letters
+const fs = require('fs');
+
+function getRandomWord() {
+    const data = fs.readFileSync('words.txt', 'utf8');
+
+    const words = data.split('\n');
+
+    const possibleWords = words.filter(word => word.length > 2);
+
+    const randomIndex = Math.floor(Math.random() * possibleWords.length);
+
+    const secretWord = possibleWords[randomIndex];
+
+    return secretWord;
+}
 
 
 
